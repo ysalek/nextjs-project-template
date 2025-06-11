eecb8dacb948f4f21a44cc0a7339cd646f50d952
ysalek <ysalek1@gmail.com>
2025-06-11 05:43:28 -0400

    Add .next/static/chunks/src_app_dashboard_agente_page_tsx_df2980a6._.js 1749635011346

---
+++ .next/static/chunks/src_app_dashboard_agente_page_tsx_df2980a6._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_8ddb124f._.js",
+  "static/chunks/node_modules_c3c0754b._.js"
+],
+    source: "dynamic"
+});
