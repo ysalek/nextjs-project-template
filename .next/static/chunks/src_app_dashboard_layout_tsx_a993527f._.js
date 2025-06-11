df6b7af31f3377a177e03aff287bf432d9de83ca
ysalek <ysalek1@gmail.com>
2025-06-11 05:43:36 -0400

    Add .next/static/chunks/src_app_dashboard_layout_tsx_a993527f._.js 1749635019026

---
+++ .next/static/chunks/src_app_dashboard_layout_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_be229867._.js",
+  "static/chunks/src_65623044._.js"
+],
+    source: "dynamic"
+});
